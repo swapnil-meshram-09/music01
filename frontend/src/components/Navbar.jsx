f77115ec89ed54b75c3a665c951b449257e925e0
swapnil-meshram-09 <swapnilmeshram514@gmail.com>
2026-01-13 16:25:06 +0530

    new commit

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import Song1 from '../Assets/Images/Tumbnail/image.png'
 function Navbar() {
   return (
     <>
-    <div className='z-10 absolute top-81 left-0 h-10 w-192 bg-black' >
+    <div className='z-10 absolute top-81 left-0 h-50 w-192 bg-black' >
         <img src="" alt="" />
     </div>
     </>
